bbe3405d63dc404b776829519cdb47bb90f831a6
pmsu2007 <pmsu2008@gmail.com>
2023-08-02 14:26:09 +0900

    feat : project init

---
+++ kkyong/src/main/java/smu/likelion/kkyong/KkyongApplication.java
@@ -0,0 +1,13 @@
+package smu.likelion.kkyong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkyongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KkyongApplication.class, args);
+	}
+
+}
